1b6523cfdf6ce13a266a833895c7aa5929a99a9e
Washington Pereira <slash.o.teobaldo@gmail.com>
2023-11-13 21:43:15 -0300

    Firts Commit

---
+++ desafio.js
@@ -0,0 +1,31 @@
+
+
+let character = "CHampa";
+
+let xp = 32000;
+
+
+
+if (xp < 1000){
+    console.log("O Heroi "+character+" esta no nível Ferro");
+}else if(xp >= 1001 && xp<= 2000){
+    console.log("O Heroi "+character+" esta no nível Bronze");
+}else if (xp >= 2001 && xp <= 5000){
+    console.log("O Heroi "+character+" esta no nível Prata");
+}else if(xp >= 6001 && xp <= 7000){
+    console.log("O Heroi "+character+" esta no nível Ouro");
+}else if(xp >= 7001 && xp<= 8000){
+    console.log("O Heroi "+character+" esta no nível Platina");
+}else if(xp >= 8001 && xp<= 9000){
+    console.log("O Heroi "+character+" esta no nível Ascendente");
+}else if(9001 && xp <= 10000){
+    console.log("O Heroi "+character+" esta no nível Imortal");
+}else if(xp >= 10001){
+    console.log("O Heroi "+character+" esta no nível Radiante");
+}
+ 
+      
+   
+
+
+   
